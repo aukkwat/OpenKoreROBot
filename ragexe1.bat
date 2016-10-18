300134b2fbd533b055a396126230622efa250c93
TheBig <thebig@thebig.local>
2016-10-19 01:56:23 +0700

    Bot สำหรับ OpenKoreRO เท่านั้น

---
+++ ragexe1.bat
@@ -0,0 +1,29 @@
+Start wxstart.exe --control=control/H
+Timeout 2
+Start wxstart.exe --control=control/P
+Timeout 2
+Start wxstart.exe --control=control/K
+timeout 2
+Start wxstart.exe --control=control/T
+timeout 2
+Start wxstart.exe --control=control/M
+timeout 2
+Start wxstart.exe --control=control/M1
+timeout 2
+Start wxstart.exe --control=control/MA
+timeout 2
+Start wxstart.exe --control=control/P1
+timeout 2
+Start wxstart.exe --control=control/P2
+timeout 2
+Start wxstart.exe --control=control/control1
+timeout 2
+Start wxstart.exe --control=control/control2
+timeout 2
+Start wxstart.exe --control=control/control3
+timeout 2
+Start wxstart.exe --control=control/control4
+timeout 2
+Start wxstart.exe --control=control/control5
+
+
